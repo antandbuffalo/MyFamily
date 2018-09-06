e3ceaa348bedbbf45fd8cb2e6b7516b820f8c66d
developer1 Jeyabalaji <developer1@Jeyabalajis-iMac.local>
2018-09-06 18:56:10 +0800

    feature: added a basic provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { MainProvider } from '../providers/main/main';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,8 @@ import { LoginPage } from '../pages/login/login';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -27,7 +32,8 @@ import { LoginPage } from '../pages/login/login';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MainProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MainProvider } from '../../providers/main/main';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  members: String [];
+  errorMessage: String;
+  constructor(public navCtrl: NavController, public mainProvider: MainProvider) {
 
   }
+
+  ionViewDidLoad() {
+    this.getMembers();
+  }
+
+  getMembers() {
+    this.mainProvider.getMembers()
+       .subscribe(
+         members => this.members = members,
+         error =>  this.errorMessage = <any>error);
+  }
+
+  items = ["Jeyaba", "asdf"];
 
 }
